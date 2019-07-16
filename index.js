0963c747f2e67c2423ca95b4021cc003a140af42
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 21:57:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!";
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name){
-  return 'Happy ${holiday}, ${name}!';
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days){
-  "It's ${days} days until ${holiday}!";
+  `It's ${days} days until ${holiday}!`;
 }
